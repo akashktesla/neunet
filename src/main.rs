f556a940cce99c385a948c02ac71dd7590e49340
akashktesla <akashktesla@gmail.com>
2023-08-11 16:32:03 +0530

    :)

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,6 @@ fn main() {
     println!("file read");
     let mut rng = rand::thread_rng();
     let mut data:Array2<f64> = df.to_ndarray::<Float64Type>(IndexOrder::C).unwrap();
-    data.as_slice_mut().unwrap().shuffle(&mut rng);
-    // println!("data: {:?}",data);
     println!("data generated");
 
     let a = data.shape();
@@ -28,35 +26,26 @@ fn main() {
     drop(a);
 
     let data_dev:Array2<f64> = data.slice(s![0..1000,..]).to_owned();
-    let y_dev = data_dev.column(0);
+    let y_dev = data_dev.column(0).to_owned();
     let x_dev = data_dev.slice(s![..,1..n]).to_owned();
-    // println!("x_dev: {:?}",x_dev);
-    // println!("y_dev: {:?}",y_dev);
+    println!("x_dev: {:?}",x_dev);
+    println!("y_dev: {:?}",y_dev);
 
     let data_train:Array2<f64> = data.slice(s![1000..m,..]).to_owned();
-    let y_train = data_train.column(0);
+    let y_train = data_train.column(0).to_owned();
     let x_train = data_train.slice(s![..,1..n]).to_owned();
     println!("data splitted");
-    // println!("x_train: {:?}",x_train);
-    // println!("y_train: {:?}",y_train);
-    let w1 = Array2::random((10,784), Uniform::new(-0.5, 0.5));
-    let b1 = Array2::random((10,1), Uniform::new(-0.5, 0.5));
-    let w2 = Array2::random((10,10), Uniform::new(-0.5, 0.5));
-    let b2 = Array2::random((10,1), Uniform::new(-0.5, 0.5));
+    println!("x_train: {:?}",x_train);
+    println!("y_train: {:?}",y_train);
     let x = x_train.row(0).to_owned();
     println!("x: {:?}",x);
-    forward_prop(w1, b1, w2, b2, x);
+    let (a1,z1,a2,z2) = forward_prop(x_train.clone()); 
     // println!("x forward: {:?}", forward_prop(w1, b1, w2, b2,x));
+    let w2 = Array2::random((10,10), Uniform::new(-0.1, 0.11));
+    back_prop(z1, a1, z2, a2, w2,x_train,y_train)
 
 }
 
-fn init_params()->(Array2<f64>,Array2<f64>,Array2<f64>,Array2<f64>){
-    let w1 = Array2::random((10,784), Uniform::new(-0.5, 0.5));
-    let b1 = Array2::random((10,1), Uniform::new(-0.5, 0.5));
-    let w2 = Array2::random((10,10), Uniform::new(-0.5, 0.5));
-    let b2 = Array2::random((10,1), Uniform::new(-0.5, 0.5));
-    return (w1,b1,w2,b2)
-}
 
 fn relu(x:Array2<f64>)->Array2<f64>{
     let vec =  x.map(|s|{ 
@@ -71,34 +60,91 @@ fn relu(x:Array2<f64>)->Array2<f64>{
 }
 
 fn softmax(x:Array2<f64>)->Array2<f64>{
-    let sum = x.map(|s|E.powf(*s)).sum();
-    return x.map(|s|E.powf(*s)/sum);
+    // println!("softmax");
+    return Array::from_iter(x.axis_iter(Axis(0)).map(|s|{
+        let es = s.map(|t|{
+            return E.powf(*t);
+        });
+        let sum = es.sum().min(f64::MAX);
+        // println!("sum: {:?}",sum);
+        let es = es.map(|t|{
+            // println!("t,sum,t/sum {:?},{:?},{:?}",t,sum,t/sum);
+            return t.min(f64::MAX)/sum;
+        });
+        return es;})
+        .flatten())
+        .into_shape((x.nrows(),x.ncols())).unwrap();
 }
 
-fn forward_prop(w1:Array2<f64>,b1:Array2<f64>,w2:Array2<f64>,b2:Array2<f64>,x:Array1<f64>)->Array2<f64>{
-    let z1 = w1.dot(&x).into_shape((10,1)).unwrap()+b1;
+fn forward_prop(x:Array2<f64>)->(Array2<f64>,Array2<f64>,Array2<f64>,Array2<f64>){
+    let w1 = Array2::random((784,10), Uniform::new(-0.1, 0.1));
+    let b1 = Array1::random((10), Uniform::new(-0.1, 0.11));
+    let w2 = Array2::random((10,10), Uniform::new(-0.1, 0.11));
+    let b2 = Array1::random((10), Uniform::new(-0.1, 0.1));
+    // let z1 = w1.dot(&x).into_shape((10,1)).unwrap()+b1;
+    let z1 = x.dot(&w1)+b1;
     println!("z1: {:?}",z1);
-    let a1 = relu(z1);
+    let a1 = relu(z1.clone());
     println!("a1: {:?}",a1);
-    let z2 = w2.dot(&a1) + b2;
+    let z2 = a1.dot(&w2) + b2;
     println!("z2: {:?}",z2);
-    let a2 = softmax(relu(z2));
+    let a2 = softmax(z2.clone());
     println!("a2: {:?}",a2);
     let mut sum = 0.0;
     for i in &a2{
         sum+=*i;
     }
     println!("sum: {}",sum);
-    return a2;
+    return (z1,a1,z2,a2);
 }
 
-fn back_prop(){
-    
+fn one_hot(y:Array1<f64>)->Array2<f64>{
+    println!("y: {:?}",y);
+    println!("y: {:?}",y.shape());
+    let r = y.shape()[0];
+    let mut one_hot_y:Array2<f64> = Array2::<f64>::zeros((r,10)); //10 is no of labels (0 to 9) la
+    for i in 0..r{
+        let val = y[i] as usize;
+        one_hot_y[(i,val)] = 1.0;
+    }
+    println!("one_hot_y: {:?}",one_hot_y);
+    return one_hot_y;
+}
+
+fn deriv_relu(z:f64)->f64{
+    if z>0.0{
+        return 1.0
+    }
+    return 0.0
+}
+
+fn back_prop(z1:Array2<f64>,a1:Array2<f64>,z2:Array2<f64>,a2:Array2<f64>,w2:Array2<f64>,x:Array2<f64>,y:Array1<f64>){
+    let m = y.ndim();
+    let one_hot_y  = one_hot(y);
+    println!("one_hot_y: {:?}",one_hot_y);
+    let dz2 = a2- one_hot_y;
+    println!("dz2 {:?}",dz2);
+    //is it not working or taking too much time (vanthu check pannu)
+    //inaikulla itha mudichitu cs50 course start paniranum... this shit is complex AF(fr now)
+    let dw2 = (1/m) as f64 * a1.dot(&dz2.t());
+    println!("dw2 {:?}",dw2);
+    // let db2 = (1/m) as f64 * dz2.sum_axis(Axis(1));
+    // println!("db2 {:?}",db2);
+    // let dz1 = dz2.dot(&w2.t());
+    // println!("dz1 {:?}",dz1);
+     
 }
 
 
 
+// fn main(){
+//     let x: Array2<f64> = Array2::from_shape_vec((2, 3), vec![
+//         100.0, 200.0, 300.0,
+//         400.0, 500.0, 1000.0,
+//     ]).unwrap();
+//     let a = softmax(x);
+//     println!("{:?}",a);
 
-
+// }
 
 
